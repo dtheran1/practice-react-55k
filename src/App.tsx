2ff54bb9eb8183587df8338c43b0319c8288b7df
Dtheran <dtheran1@gmail.com>
2023-04-13 21:52:55 -0500

    add component UsersList in table

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-// import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { type User } from './types'
+import { UsersList } from './components/UsersList'
 
 function App () {
-  // const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      .then(async res => await res.json())
+      .then(res => {
+        setUsers(res.results)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
 
   return (
     <div className="App">
       <h1>Prueba Tecnica</h1>
+
+      <UsersList users={users} />
     </div>
   )
 }
