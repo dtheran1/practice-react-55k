7588efc3be0448c9015f9bfd0852875405a7b593
Dtheran <dtheran1@gmail.com>
2023-04-17 10:53:04 -0500

    implement useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import { type User } from './types'
 import { UsersList } from './components/UsersList'
@@ -7,17 +7,7 @@ function App () {
   const [users, setUsers] = useState<User[]>([])
   const [showColors, setShowColors] = useState(false)
   const [shortByContry, setshortByContry] = useState(false)
-
-  useEffect(() => {
-    fetch('https://randomuser.me/api?results=100')
-      .then(async res => await res.json())
-      .then(res => {
-        setUsers(res.results)
-      })
-      .catch(err => {
-        console.error(err)
-      })
-  }, [])
+  const originalUsers = useRef<User[]>([]) // Este hook se utiliza para almacenar valores que persisten a través de un renderizado y no causan una actualización del componente.
 
   const toggleColors = () => {
     setShowColors(!showColors)
@@ -26,6 +16,27 @@ function App () {
   const toggleShortByCountry = () => {
     setshortByContry(prevState => !prevState)
   }
+
+  const handleReset = () => {
+    setUsers(originalUsers.current)
+  }
+
+  const handleDelete = (email: string) => {
+    const filteredUsers = users.filter((user, userIndex) => user.email !== email)
+    setUsers(filteredUsers)
+  }
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      .then(async res => await res.json())
+      .then(res => {
+        setUsers(res.results)
+        originalUsers.current = res.results
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
 
   const shortedUsers = shortByContry
     ? users.toSorted((a, b) => {
@@ -39,8 +50,11 @@ function App () {
       <header>
         <button onClick={toggleColors}>Colorear filas</button>
         <button onClick={toggleShortByCountry}>{shortByContry ? 'No ordenar por pais' : 'Ordenar por pais' }</button>
+        <button onClick={handleReset}>
+          Resetear estado
+        </button>
       </header>
-      <UsersList showColors={showColors} users={shortedUsers} />
+      <UsersList showColors={showColors} users={shortedUsers} deleteUser={handleDelete} />
     </div>
   )
 }
--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,11 +1,12 @@
 import { type User } from '../types'
 
 interface Props {
-  users: User[]
+  deleteUser: (email: string) => void
   showColors: boolean
+  users: User[]
 }
 
-export function UsersList ({ users, showColors }: Props) {
+export function UsersList ({ deleteUser, users, showColors }: Props) {
   return (
     <table width='100%'>
       <thead>
@@ -25,7 +26,7 @@ export function UsersList ({ users, showColors }: Props) {
             const color = showColors ? backgroudColor : 'transparent'
 
             return (
-              <tr key={index} style={{ backgroundColor: color }}>
+              <tr key={user.email} style={{ backgroundColor: color }}>
                 <td>
                   <img src={user.picture.thumbnail} alt="" />
                 </td>
@@ -33,7 +34,7 @@ export function UsersList ({ users, showColors }: Props) {
                 <td>{user.name.last}</td>
                 <td>{user.location.country}</td>
                 <td>
-                  <button>Borrar</button>
+                  <button onClick={() => { deleteUser(user.email) }}>Borrar</button>
                 </td>
 
               </tr>
