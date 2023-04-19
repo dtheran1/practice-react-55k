740c07556bbb98e4ae99c576dcbde4b7375ba137
Dtheran <dtheran1@gmail.com>
2023-04-18 19:42:57 -0500

    ordenar por

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect, useRef, useMemo } from 'react'
 import './App.css'
-import { type User } from './types'
+import { SortBy, type User } from './types.d'
 import { UsersList } from './components/UsersList'
 
 function App () {
   const [users, setUsers] = useState<User[]>([])
   const [showColors, setShowColors] = useState(false)
-  const [shortByContry, setshortByContry] = useState(false)
+  const [sorting, setSorting] = useState<SortBy>(SortBy.NONE)
   const originalUsers = useRef<User[]>([]) // Este hook se utiliza para almacenar valores que persisten a través de un renderizado y no causan una actualización del componente.
   const [filterCountry, setFilterCountry] = useState<string | null>(null)
   const toggleColors = () => {
@@ -14,7 +14,8 @@ function App () {
   }
 
   const toggleShortByCountry = () => {
-    setshortByContry(prevState => !prevState)
+    const newSotingValue = sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE
+    setSorting(newSotingValue)
   }
 
   const handleReset = () => {
@@ -24,6 +25,10 @@ function App () {
   const handleDelete = (email: string) => {
     const filteredUsers = users.filter((user, userIndex) => user.email !== email)
     setUsers(filteredUsers)
+  }
+
+  const handleChangeSort = (sort: SortBy) => {
+    setSorting(sort)
   }
 
   useEffect(() => {
@@ -45,23 +50,31 @@ function App () {
   }, [users, filterCountry])
 
   const sortedUsers = useMemo(() => {
-    return shortByContry
-      ? filteredUsers.toSorted((a, b) => a.location.country.localeCompare(b.location.country))
-      : filteredUsers
-  }, [filteredUsers, shortByContry])
+    if (sorting === SortBy.NONE) return filteredUsers
+    const compareProperties: Record<string, (user: User) => any> = {
+      [SortBy.COUNTRY]: user => user.location.country,
+      [SortBy.NAME]: user => user.name.first,
+      [SortBy.LAST]: user => user.name.last
+    }
+
+    return filteredUsers.toSorted((a, b) => {
+      const extractProperty = compareProperties[sorting]
+      return extractProperty(a).localeCompare(extractProperty(b))
+    })
+  }, [filteredUsers, sorting])
 
   return (
     <div className="App">
       <h1>Prueba Tecnica</h1>
       <header>
         <button onClick={toggleColors}>Colorear filas</button>
-        <button onClick={toggleShortByCountry}>{shortByContry ? 'No ordenar por pais' : 'Ordenar por pais' }</button>
+        <button onClick={toggleShortByCountry}>{sorting === SortBy.COUNTRY ? 'No ordenar por pais' : 'Ordenar por pais' }</button>
         <button onClick={handleReset}>
           Resetear estado
         </button>
         <input type="text" placeholder='Filtra por pais' onChange={(e) => { setFilterCountry(e.target.value) }} />
       </header>
-      <UsersList showColors={showColors} users={sortedUsers} deleteUser={handleDelete} />
+      <UsersList changeSorting={handleChangeSort} showColors={showColors} users={sortedUsers} deleteUser={handleDelete} />
     </div>
   )
 }
